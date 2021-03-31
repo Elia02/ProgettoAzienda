9fa5bb040b4522522cabd60c908d5297de8d23c0
nichoballe02 <65011098+nichoballe02@users.noreply.github.com>
2021-03-31 07:57:31 +0200

    Add files via upload

---
+++ LavoriInCorso/PHP/Export.php
@@ -0,0 +1,18 @@
+<?php  
+      //export.php  
+ 
+      $connect = mysqli_connect("localhost", "root", "", "dbfinale");  
+      header('Content-Type: text/csv; charset=utf-8');  
+      header('Content-Disposition: attachment; filename=data.csv');  
+      $output = fopen("php://output", "w");  
+      fputcsv($output, array('id','ANNOSCOLASTICO','AREAGEOGRAFICA','REGIONE','PROVINCIA','CODICEISTITUTORIFERIMENTO','DENOMINAZIONEISTITUTORIFERIMENTO','CODICESCUOLA','DENOMINAZIONESCUOLA','INDIRIZZOSCUOLA','CAPSCUOLA','CODICECOMUNESCUOLA','DESCRIZIONECOMUNE','DESCRIZIONECARATTERISTICASCUOLA','DESCRIZIONETIPOLOGIAGRADOISTRUZIONESCUOLA','INDICAZIONESEDEDIRETTIVO','INDICAZIONESEDEOMNICOMPRENSIVO','INDIRIZZOEMAILSCUOLA','INDIRIZZOPECSCUOLA','SITOWEBSCUOLA','SEDESCOLASTICA'));  
+      $query = "SELECT * from prova";  
+      $result = mysqli_query($connect, $query);  
+      while($row = mysqli_fetch_assoc($result))  
+      {  
+           fputcsv($output, $row);  
+      }  
+      fclose($output);  
+  
+ ?>  
+
